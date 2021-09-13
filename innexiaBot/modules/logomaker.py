e7e3f0e0266c508d6e67c86dbabe49da60907a0c
ROCKSTAR OP <88956892+T5FR0CK5T4R@users.noreply.github.com>
2021-09-13 17:07:33 +0530

    Update logomaker.py

--- innexiaBot/modules/logomaker.py
+++ innexiaBot/modules/logomaker.py
@@ -100,9 +100,9 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @SiderzBot Support @Florenza_support")
+    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @SiderzBot_Support")
     if os.path.exists(fname2):
-            os.remove(fname2)
+            os.remove(f
  except Exception as e:
    await event.reply(f'Error Report hhj;@TSF_R0CKSTAR , {e}')
 
@@ -144,7 +144,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Florenza_support , {e}')
+   await event.reply(f'Error Report @TSF_R0CKSTAR , {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
